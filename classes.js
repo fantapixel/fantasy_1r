c548fe506882d5211cc32c280f6af068e61ee9c2
Daniel <d.motrenko2010@gmail.com>
2023-06-07 17:58:11 +0300

    classes added

--- classes.js
+++ classes.js
@@ -1,13 +1,15 @@
-class Rectangle {
-    constructor(a, b) {
+class Triangle {
+    constructor(a, b, c) {
         this.a = a;
         this.b = b;
+        this.c = c;
     }
-}
-
-
     square() {
         return this.a * this.b
     }
-let rect = new Rectangle(2,4);
-console.log(rect.square);
+    p() {
+        return this.a + this.b + this.c
+    }
+}
+let rect = new Triangle(2,4,6.4);
+console.log(rect.p());
